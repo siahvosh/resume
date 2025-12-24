67299f3bea6d0a1988cee93c3e3b37fa0a10805b
siavash <siablack370@gmail.com>
2025-12-24 20:57:34 +0330

    set dialog text and title

--- src/component/dialog/experience.tsx
+++ src/component/dialog/experience.tsx
@@ -17,25 +17,39 @@ export default function AlertDialog({ open, onClose }) {
     return (
         <Dialog
             open={open}
+            sx={{
+                background: '#000000',
+                '& .MuiPaper-root': {
+                    background: '#000000',
+                },
+                '& .MuiBackdrop-root': {
+                    backgroundColor: 'transparent',
+
+                },
+
+            }}
+            style={{ opacity: 0.9,}}
             onClose={onClose}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
         >
-            <DialogTitle id="alert-dialog-title">
-                {"Use Google's location service?"}
+            <DialogTitle style={{color: 'white', fontWeight: '900'}} id="alert-dialog-title">
+                {"At Derak Cloud"} <br/>
+                <span style={{fontSize: '14px', fontWeight: '100'}}>August 2022 - March 2025 </span>
             </DialogTitle>
 
             <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Let Google help apps determine location. This means sending anonymous
-                    location data to Google, even when no apps are running.
+                <DialogContentText style={{color: 'white'}} id="alert-dialog-description">
+                    .Led the responsive design and implementation of user and admin panels, optimizing dashboards, purchase and renewal flows, and mobile access to support users and administrators across all device types.
                 </DialogContentText>
+
+
             </DialogContent>
 
             <DialogActions>
-                <Button onClick={onClose}>Disagree</Button>
+
                 <Button onClick={onClose} autoFocus>
-                    Agree
+                    read
                 </Button>
             </DialogActions>
         </Dialog>
