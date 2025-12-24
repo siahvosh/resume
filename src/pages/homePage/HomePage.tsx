0e6609583216c1b1341d5c832914f83cf63ce6aa
siavash <siablack370@gmail.com>
2025-12-24 16:11:06 +0330

    remove useless functions in js

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -5,12 +5,6 @@ import img from "../../assets/phone1.png";
 import img2 from "../../assets/img2.png";
 import img3 from "../../assets/img3.png";
 import img4 from "../../assets/img4.png";
-import img5 from "../../assets/2x/Asset 9@2x.png";
-import img6 from "../../assets/4x/dot2.png";
-import img7 from "../../assets/2x/Asset 9@2x.png";
-import img8 from "../../assets/2x/Asset 9@2x.png";
-import bgImg from "../../assets/bgnew.jpg";
-import videoCode from "../../assets/videoCode.mp4";
 import skater from '../../assets/2skater.png'
 
 import Lottie from "lottie-react";
@@ -25,43 +19,14 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 import './HomePage.css'
 import './../../App.css'
-import {useNavigate} from "react-router-dom";
-import {Grid} from "@visx/visx";
-import {Tollbar} from "../../component/tollbar/Tollbar";
+
 
 
 
 export const HomePage = () => {
-    const [rotate, setRotate] = useState({ x: 0, y: 0 });
-
-    const handleMouseMove = (e) => {
-        const x = (window.innerWidth / 2 - e.clientX) / 25;
-        const y = (window.innerHeight / 2 - e.clientY) / 25;
-        setRotate({ x, y });
-    };
-
 
     const [activeIndex, setActiveIndex] = useState(0);
     const cont2Ref = useRef(null);
-    const navigate = useNavigate();
-    const [stepperDetail] = useState([
-        {title: 'Responsive', description: '- Design and development of a responsive version of the company website for a better user experience on mobile devices\n' +
-                '- Add key additional sections( buy and renewal of services) of the website on mobile devices\n' +
-                '- Optimizing the billing section in performance and user experience of the website on mobile devices\n', image: `${img}`, image1: `${img5}`, path: '/section1'},
-        {title: 'Refactor UI Project', description: '- Refactoring and optimizing significant sections of the front-end project\n' +
-                '\n' +
-                '-\tSolving different issues and code bugs, eliminate redundant functions, optimizing load times by up to 40%\n', image: `${img2}`, image1: `${img6}`, path: '/section2'},
-        {title: 'Onboarding', description: '- Design and implementation of the onboarding process for new employees in front-end and UI/UX team  \n' +
-                '\n' +
-                '- Introducing new members to various components of the project, development workflows, and coding standards to accelerate integration and productivity.\n' +
-                '\n' +
-                '- Training new employees, transferring the hands-on experience to reduce time-to-productivity for new team members.\n', image: `${img3}`, image1: `../../assets/dot.png`, path: '/section3'},
-        {title: 'BI Dashboard', description: '-Development of the business intelligence analytics dashboard for sales, purchase, human resource, customer finance team\n' +
-                '\n' +
-                '-\tImplementation of live charts and dashboards. There are manual KPIs and AI generated KPIs with real word data of the different businesses\n', image: `${img4}`, image1: `../../assets/dot.png`, path: '/section4'},
-        {title: 'End', description: 'Axis independently, without needing Lorem ipsum dolor sit amet.', image: `${img4}`, image1: `../../assets/dot.png`, path: '/section5'},
-    ]);
-
     const stepsVisuals = [
         {
             id: 0,
@@ -210,17 +175,6 @@ export const HomePage = () => {
         },
     ];
 
-
-
-    const detailHandler = (idx: any) => {
-        console.log({idx:idx})
-        setActiveIndex(idx);
-    }
-
-    const handelNav = (path: any) => {
-        // console.log(path);
-        navigate(path);
-    }
 
     useEffect(() => {
     const handleScroll = () => {
@@ -430,77 +384,3 @@ export const HomePage = () => {
     )
 }
 
-
-
-
-// <Grid2 size={{xs: 12, sm: 12, md: 12, lg: 12}}>
-//     <div className={'text-box-animation'}>
-//         <div style={{width: '45vw'}}>
-//             {stepsVisuals.map((item, index) => (
-//                 <div key={index} onClick={() => detailHandler(index)}>
-//                     <div key={index} onClick={() => setActiveIndex(index)}>
-//                                             <span className={`text-title ${activeIndex === index ? 'active' : ''}`}>
-//                                                 {item.title}
-//                                             </span>
-//                         <div style={{ flexDirection: 'column'}} className={`details ${activeIndex === index ? 'show' : ''}`}>
-//                             {item.description}
-//                         </div>
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-//     </div>
-// </Grid2>
-// <Grid2 size={{xs: 12, sm: 12, md: 12, lg: 12}}>
-//     <div className={'images-box'}>
-//         <AnimatePresence>
-//             {stepsVisuals.slice(0, activeIndex + 1).map((item, index) => (
-//                 <motion.img
-//                     key={item.id}
-//                     src={item.background.src}
-//                     alt=""
-//                     className="img"
-//                     style={{
-//
-//                         // marginTop: -10,
-//                         // zIndex: index,
-//                         skewX: 0,
-//                     }}
-//                     initial={{
-//                         opacity: 0,
-//                         scale: 1,
-//                         rotate: 0,
-//                         skewX: 0,
-//                         translateY: 200,
-//                         skew: 0
-//                     }}
-//                     animate={{
-//                         opacity: index === activeIndex ? 1 : 0,
-//                         scale: 1.3,
-//                         rotate: 10,
-//                         skewX: 0,
-//                         translateY: 0,
-//                         skew: 0,
-//                         y: [0, -15, 0] ,
-//                     }}
-//                     exit={{
-//                         opacity: 0,
-//                         scale: 1.1,
-//                         skewX: 0,
-//                         rotate: 0,
-//                         translateY: 200,
-//                         skew: 0
-//                     }}
-//                     transition={{duration: 0.5,y: {
-//                             duration: 3.5,
-//                             repeat: Infinity,
-//                             ease: "easeInOut",
-//                         },}}
-//                 />
-//
-//
-//             ))}
-//         </AnimatePresence>
-//
-//     </div>
-// </Grid2>
